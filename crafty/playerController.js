fd84ad6c0f90047d593e9e169e0263cab1e7a1cb
neekey <ni184775761@gmail.com>
2011-06-14 11:31:18 +0800

    save project

--- crafty/playerController.js
+++ crafty/playerController.js
@@ -18,6 +18,7 @@ Crafty.c( 'playerController', {
 		else  {	
 			Crafty.$e.log('setPlayerAction: 参数有误！');
 		}
+		return this;
 	},
 	
 	/**
@@ -36,6 +37,7 @@ Crafty.c( 'playerController', {
 		else {
 			Crafty.$e.log('doPlayerAction: 当前控制玩家不存在或者未指定执行的动作！');
 		}
+		return this;
 	},
 	
 	/**
@@ -48,6 +50,52 @@ Crafty.c( 'playerController', {
 		}
 		else {
 			Crafty.$e.log('setPlayer: 参数有误！');
+		}
+		return this;
+	},
+	
+	/**
+	 * 设置行动角色顺序队列
+	 * @param {Array}
+	 */
+	setPlayerQueue: function( queue ){
+		if( Crafty.$getType( queue ) == 'Array' ){
+			this._playerQueue = queue;
+		}
+		else {
+			Crafty.$e.log('setPlayerQueue: 参数有误！');
+		}
+		return this;
+	},
+	
+	/**
+	 * 控制器启动，该方法仅适用于需要角色行动顺序队列的状态
+	 */
+	controllerLaunch: function(){
+		if( this._playerQueue && this._playerQueue.length > 0 ){
+			this.bind('playerTurnChange', this.playerLoop );
+			this.trigger('playerTurnChange');
+		}
+		return this;
+	},
+	
+	/**
+	 * 玩家控制队列主循环
+	 * @private
+	 */
+	_playerLoop: function(){
+		if( this._playerQueue && this._playerQueue.length > 0 ){
+			// 更换角色控制
+			// 取出队列中的第一个角色作为控制角色
+			var curP = this._playerQueue[ 0 ];
+			// 将该角色添加到队尾
+			this._playerQueue.push( curP );
+			this.setPlayer( curP );
+			// 触发角色激活事件
+			this.trigger('playerActived');
+		}
+		else {
+			this.unbind('playerTurnChange', this.playerLoop );
 		}
 	}
 });
