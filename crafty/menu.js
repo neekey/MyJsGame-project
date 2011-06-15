fb5a1322a6bcad9cec4a7602df3140e0601aefcf
neekey <ni184775761@gmail.com>
2011-06-15 19:45:22 +0800

    fix some bug, but still not good enough, you can not specified padding in your css files

--- crafty/menu.js
+++ crafty/menu.js
@@ -1,4 +1,7 @@
-﻿
+﻿/**
+ * 菜单组件，暂时无法使用css定义padding等属性，可以定义background
+ */
+
 Crafty.c('menuUnit', {
 	
 	/**
@@ -24,7 +27,7 @@ Crafty.c('menuUnit', {
 				}
 			}
 		})
-		.attr({w: 200, h: 50, z: 1000 });
+		.attr({w: 100, h: 25, z: 1000 });
 	},
 	
 	/**
@@ -51,6 +54,9 @@ Crafty.c('menuUnit', {
 		this.css( 'color', colorValue );
 	},
 	
+	/**
+	 * 显示菜单单元
+	 */
 	showMenuUnit: function(){
 		if( !this.attr('visible') ){
 			this.attr('visible', true);
@@ -59,6 +65,9 @@ Crafty.c('menuUnit', {
 		}
 	},
 	
+	/**
+	 * 隐藏菜单单元，若子菜单存在，将同时隐藏子菜单
+	 */
 	hideMenuUnit: function(){
 		this.attr('visible', false);
 		// 隐藏子菜单
@@ -69,11 +78,15 @@ Crafty.c('menuUnit', {
 		this.unbind('mouseout', this._menuUnitMouseout);
 	},
 	
+	/**
+	 * mouseover 事件
+	 * @private
+	 */
 	_menuUnitMouseover: function(){
 		if( this._childMenu && this._childMenu.has && this._childMenu.has('menu') ){
 			// 设置子菜单位置
 			this._childMenu.attr({
-				x: this.attr('x') - 100 + this.attr('w'),
+				x: this.attr('x') + this.attr('w'),
 				y: this.attr('y')
 			});
 			// 展开子菜单
@@ -81,6 +94,10 @@ Crafty.c('menuUnit', {
 		}
 	},
 	
+	/**
+	 * mouseout 事件
+	 * @private
+	 */
 	_menuUnitMouseout: function( e ){
 		if( this._childMenu && this._childMenu.has && this._childMenu.has('menu') ){
 			// 判断是否在子菜单上
@@ -100,7 +117,7 @@ Crafty.c('menuUnit', {
 Crafty.c('menu', {
 	
 	_menuList: null,
-	_menuUpdateOffset: 10,
+	_menuUpdateOffset: 5,
 	_menuUpdateOffsetFrame: 0,
 	
 	init: function(){
@@ -111,10 +128,16 @@ Crafty.c('menu', {
 	
 	/**
 	 * 初始化餐单
+	 * @param {Objcet} m
+	 * @param {String} className
 	 */
-	menuSetup: function( m ){
+	menuSetup: function( m, className ){
 		if( Crafty.$getType( m ) == 'Object' ){
-			this._buildMenu( m, this );
+			this._buildMenu( m, this, className );
+			// 添加class
+			if( className ){
+				this._element.className = this._element.className + ' ' + className;
+			}
 		}
 		else {
 			Crafty.$e.log('menuSetup: 参数有误！');
@@ -126,11 +149,13 @@ Crafty.c('menu', {
 	 * 显示菜单
 	 */
 	showMenu: function(){
+		this.hideMenu();
 		this.attr('visible', true);
 		// 隐藏所有菜单单元
 		this.forEachMenuUnit( function(){
 			this.showMenuUnit();
 		});
+		return this;
 	},
 	
 	/**
@@ -141,6 +166,7 @@ Crafty.c('menu', {
 		this.forEachMenuUnit( function(){
 			this.hideMenuUnit();
 		});
+		return this;
 	},
 	
 	/**
@@ -164,7 +190,8 @@ Crafty.c('menu', {
 	/**
 	 * 根据给定的菜单信息，递归构造菜单
 	 */
-	_buildMenu: function( m, scope ){
+	_buildMenu: function( m, scope, className ){
+		
 		// 初始化餐单的长宽
 		scope.attr({w: 0, h: 0 });
 		
@@ -177,10 +204,15 @@ Crafty.c('menu', {
 			unit = Crafty.e('menuUnit')
 			.text( name );
 			scope._menuList[ name ] = unit;
+			// 添加class
+			unit._element.className = unit._element.className + ' ' + className + 'Child';
 			
+			// 添加到菜单容器中
+			this._element.appendChild( unit._element );
 			// 修改菜单尺寸并调整菜单单元位置
 			// 此处将餐单单元的左上角坐标设置为菜单的左下角坐标
-			unit.attr({x: scope.attr('x'), y: scope.attr('y') + scope.attr('h') });
+			//unit.attr({x: scope.attr('x'), y: scope.attr('y') + scope.attr('h') });
+			unit.attr({x: scope.attr('x'), y: scope.attr('h') + scope.attr('y') });
 			// 增加长宽
 			scope.attr({
 				w: scope.attr('w') < unit.attr('w') ? unit.attr('w') : scope.attr('w'),
@@ -194,12 +226,14 @@ Crafty.c('menu', {
 				// 若为一个子菜单
 				if( Crafty.$getType( m[ name ] ) == 'Object' ){
 					unit._childMenu = Crafty.e('menu');
+					//unit._element.className = unit._element.className + ' ' + className
 					// 设置子菜单位置
 					unit._childMenu.attr({
-						x: unit.attr('x') - 100 + unit.attr('w'),
+						x: unit.attr('x') + unit.attr('w'),
 						y: unit.attr('y')
 					});
-					arguments.callee( m[ name ], unit._childMenu );
+					unit._childMenu.menuSetup( m[ name ], className );
+					//arguments.callee( m[ name ], unit._childMenu, className );
 				}
 			}
 		}
@@ -210,17 +244,22 @@ Crafty.c('menu', {
 	 * @private
 	 */
 	_updateMenuUnitPos: function(){
+		
 		if( this._menuUpdateOffsetFrame == this._menuUpdateOffset ){
 			// 遍历菜单单元，修正其位置
 			// 将菜单的尺寸初始化为0
 			this.attr({w: 0, h: 0});
-			var name;
+			var name, pdTop, pdRight, pdBottom, pdLeft, 
+			ChildPdTop, ChildPdRight, ChildPdBottom, ChildPdLeft,
+			childW, childH;
 			for( name in this._menuList ){
+				
 				// 此处将餐单单元的左上角坐标设置为菜单的左下角坐标
 				this._menuList[ name ].attr({x: this.attr('x'), y: this.attr('y') + this.attr('h') });
 				// 增加长宽
+				
 				this.attr({
-					w: this.attr('w') < this._menuList[ name ].attr('w') ? this._menuList[ name ].attr('w') : this.attr('w'),
+					w: ( this.attr('w') < this._menuList[ name ].attr('w') ? this._menuList[ name ].attr('w') : this.attr('w') ),
 					h: this.attr('h') + this._menuList[ name ].attr('h')
 				});
 			}
