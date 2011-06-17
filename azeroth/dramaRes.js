46bc6a5a271348685948b420c97b582d186a6c70
neekey <ni184775761@gmail.com>
2011-06-17 23:25:29 +0800

    add extend for storing drama script

---
+++ azeroth/dramaRes.js
@@ -0,0 +1,182 @@
+﻿/**
+ * 战斗前对话
+ */
+
+Crafty.drama( 'beforeFight',  [
+	{
+		name: '安东',
+		face: 'depressed',
+		text: '哼，亨利他们这次任务不也没成功吗。就因为我还丢了契约书就嘲笑我，真是可恶。',
+		action: 'show',
+		pos: 'left'
+	},
+	{
+		name: '安东',
+		face: 'depressed',
+		text: '我就不信了,将来我一定要成为像爸爸那样伟大的佣兵,连国王都要为我颁发奖章！',
+		action: 'hide',
+		pos: 'left'
+	},
+	{
+		name: '夏尔蒂',
+		face: 'sad',
+		text: '马已经跑不动了, 明明盖迪尔联邦就在眼前...可恶',
+		action: 'show',
+		pos: 'left'
+	},
+	{
+		name: '维纶骑士',
+		face: 'smile',
+		text: '哈哈!公主你还是不要跑了,集市买的马就算你骑术再好怎么跑得过我们的军马! ',
+		action: 'show',
+		pos: 'right'
+	},
+	{
+		name: '维纶骑士',
+		face: 'smile',
+		text: '您还是老老实实和我们回去吧, 我们保证不伤害你~ ',
+		action: 'hide',
+		pos: 'right'
+	},
+	{
+		name: '维纶军官',
+		face: 'normal',
+		text: '不要和她罗嗦了，上面的命令可不是让我们“接”她回去',
+		action: 'show',
+		pos: 'right'
+	},
+	{
+		name: '维纶军官',
+		face: 'normal',
+		text: '快点解决她 这边已经是盖迪尔联邦的边境了 拖久了可就麻烦了',
+		action: 'hide',
+		pos: 'right'
+	},
+	{
+		name: '维纶骑士',
+		face: 'smile',
+		text: '额...真是不懂的情调的家伙 公主您也听到了 可不要怪我没有风度啊 蛤蛤蛤蛤蛤 ',
+		action: 'show',
+		pos: 'right'
+	},
+	{
+		name: '夏尔蒂',
+		face: 'sad',
+		text: '哼！不要废话了，我是不会和你们回去的！ 真要打起来我也不会怕了你们！',
+		action: 'hide',
+		pos: 'left'
+	},
+	{
+		name: '安东',
+		face: 'surprised',
+		text: '好像有人在欺负美女啊...这种英雄救美的俗套情节竟然被我遇上了 不行不能让这些兵痞子欺负美女 ',
+		action: 'show',
+		pos: 'left'
+	},
+	{
+		name: '安东',
+		face: 'serious',
+		text: '不行不能让这些兵痞子欺负美女 ',
+		pos: 'left'
+	},
+	{
+		name: '安东',
+		face: 'serious',
+		text: '喂！你们是哪个国家的骑士 这里已经是盖迪尔联邦的领土了 ',
+		pos: 'left'
+	},
+	{
+		name: '安东',
+		face: 'serious',
+		text: '除了联邦授权的佣兵团和联邦军队 任何携带武器的人都是不受欢迎的 识相的就快点给我滚！ ',
+		pos: 'left'
+	},
+	{
+		name: '维纶骑士',
+		face: 'angry',
+		text: '我去 你丫一毛都没长全的小屁孩知道什么啊 还有对面那个女的也拿着武器呢 你怎么不说她啊 ',
+		pos: 'right'
+	},
+	{
+		name: '安东',
+		face: 'smile',
+		text: '有吗？哪里？噢 你说她啊 人家那把佩剑是装饰用的了 怎么能算是武器呢！ ',
+		action: 'hide',
+		pos: 'left'
+	},
+	{
+		name: '夏尔蒂',
+		face: 'sad',
+		text: '。。。。。。。。',
+		action: 'show',
+		pos: 'left'
+	},
+	{
+		name: '维纶骑士',
+		face: 'depressed',
+		text: '。。。。。。 ',
+		action: 'hide',
+		pos: 'right'
+	},
+	{
+		name: '维纶军官',
+		face: 'normal',
+		text: '小子 既然你找死 那就不要怪我们心狠手辣了 喂 快点解决他带走公主省的夜长梦多',
+		action: 'show',
+		pos: 'right'
+	}
+]);
+
+/**
+ * 战斗后对话
+ */
+Crafty.drama('afterFigher', [
+	{
+		name: '维纶骑士',
+		face: 'angry',
+		text: '可恶 算你们狠 我们的大部队马上就要到了 到时候 就把你们一起收拾了 有种就不要跑！哼',
+		pos: 'right'
+	},
+	{
+		name: '安东',
+		face: 'surprised',
+		text: '自己跑得比谁都快还要叫别人不要跑... ',
+		pos: 'right'
+	},
+	{
+		name: '夏尔蒂',
+		face: 'shy',
+		text: '这次多亏了你了 不然真是不知道怎么办了',
+		pos: 'right'
+	},
+	{
+		name: '安东',
+		face: 'shy',
+		text: '额 其实 我发现你自己都能应付那两个人 我纯粹打酱油了... ',
+		pos: 'right'
+	},
+	{
+		name: '夏尔蒂',
+		face: 'normal',
+		text: '..还是要谢谢你 对了刚听你说话 你认识联邦里的佣兵吗',
+		pos: 'right'
+	},
+	{
+		name: '安东',
+		face: 'smile',
+		text: '哈哈 我就是疾风之狼的呢 疾风之狼你听过吧 很给力的！ ',
+		pos: 'right'
+	},
+	{
+		name: '夏尔蒂',
+		face: 'smile',
+		text: '太好了 那你能带我去见你们的团长吗 我有事想拜托他',
+		pos: 'right'
+	},
+	{
+		name: '安东',
+		face: 'smile',
+		text: '好！没问题！跟我走吧！ ',
+		pos: 'right'
+	}
+]);
---
+++ crafty/drama.js
@@ -0,0 +1,389 @@
+﻿/**
+ * 剧情组件
+ */
+ 
+Crafty.extend({
+	drama: ( function(){
+		var _drama = {};
+		return function( name, drama ){
+			if( arguments.length == 1 ){
+				if( name in _drama ){
+					return _drama[ name ];
+				}
+			}
+			else if ( arguments.length == 2 ){
+				_drama[ name ] = drama;
+			}
+		};
+	})()
+});
+/**
+ * 剧情人物
+ */
+Crafty.c('dramaChar', {
+	
+	/**
+	 * 角色的所有表情
+	 */
+	_face: null,
+	_curFace: null,
+	_charName: null,
+	
+	
+	init: function(){
+		this.requires('2D, Canvas, attrSprite');
+		this.bind('faceChange', this._onFaceChange );
+		this.attr({w: 323, h: 482, z: 9999, x: 700, y: 100} );
+	},
+	
+	/**
+	 * 初始化 设置角色表情
+	 * @param {Object} face
+	 */
+	dramaCharSetup: function( face, name ){
+		if( Crafty.$getType( face ) == 'Object' ){
+			this._face = face;
+			this._charName = name;
+		}
+		else {
+			Crafty.$e.log('dramaCharSetup: 参数有误！');
+		}
+		return this;
+	},
+	
+	/**
+	 * 设置角色表情 
+	 */
+	setFace: function( name ){
+		
+		if( name in this._face ){
+			
+			this.removeComponent( this._curFace );
+			this.addComponent( this._face[ name ] );
+			// 取消当前的表情
+			this.trigger('faceChange');
+			
+		}
+		else {
+			// 指定的表情不存在
+			Crafty.$e.log('setFace: 指定的表情不存在');
+		}
+		return this;
+	},
+	
+	/**
+	 * 表情改变时出发事件
+	 * 添加渐隐效果
+	 */
+	_onFaceChange: function(){
+		this.attrSprite({ alpha: 0.7 }, 200 )
+		.attrSprite({ alpha: 1 }, 200 );
+		
+	},
+	
+	/**
+	 * 角色消失
+	 */
+	charFadeOut: function( callback ){
+		this.attrSprite({ alpha: 0 }, 1000, callback);
+		return this;
+	},
+	
+	/**
+	 * 角色出现
+	 */
+	charFadeIn: function( callback ){
+		this.attrSprite({ alpha: 1 }, 500, callback );
+		return this;
+	}
+	
+});
+
+Crafty.c('dramaDialog', {
+	
+	_curSpeaker: null,
+	_speakerNameEntity: null,
+	
+	init: function(){
+		this.requires('2D, DOM, Text, Color');
+		this._speakerNameEntity = Crafty.e( '2D, DOM, Text, Color' ).
+		attr({ w: 60, h: 20, x: 10, y: 340, z: 10000 })
+		.color('#ffffff')
+		.css('padding', 10);
+		
+	},
+	
+	/**
+	 * 初始化，可以为dialog的dom设置id值
+	 */
+	dramaDialogSetup: function( domId, speakerId ){
+		if( domId ){
+			this._element.id = domId;
+		}
+		if( speakerId ){
+			this._speakerNameEntity._element.id = speakerId;
+		}
+	},
+	
+	setDialog: function( speaker, content, pos ){
+		if( speaker.has && speaker.has( 'dramaChar' ) ){
+			this._curSpeaker = speaker;
+			this.text( content );
+			this.updateSpeakerName( pos );
+		}
+		else {
+			Crafty.$e.log('setDialog: 参数有误！');
+		}
+	},
+	
+	updateSpeakerName: function( pos ){
+		var s = this._curSpeaker,
+		name = s._charName;
+		this._speakerNameEntity.text( name );
+		if( pos == 'right' ){
+			this._speakerNameEntity.attr({x:810});
+		}
+		else {
+			this._speakerNameEntity.attr({x:10});
+		}
+		
+	},
+	
+	hideDialog: function(){
+		this.attr('visible', false);
+		if( this._speakerNameEntity ){
+			this._speakerNameEntity.attr('visible', false);
+		}
+	},
+	showDialog: function(){
+		this.attr('visible', true);
+		if( this._speakerNameEntity ){
+			this._speakerNameEntity.attr('visible', true);
+		}
+	}
+});
+
+/**
+ * 剧情组件
+ */
+/*
+{	
+name: 'neekey',
+face: 'smile',
+text: 'hello',
+action: show/hide,
+pos: left
+}
+*/
+Crafty.c('drama', {
+	_dramaCharList: null,
+	_speaker: null,
+	_dramaScript: null,
+	_dramaBgSprite: null,
+	_dramaText: null,
+	_curDramaIndex: 0,
+	_curLeftChar: null,
+	_curRightChar: null,
+	_isDramaPlaying: false,
+	_dramaBeginEvent: null,
+	_dramaFinishEvent: null,
+	
+	init: function(){
+		this._dramaCharList = {};
+		var rect = Crafty.viewport.rect();
+		// 设置背景组件
+		this._dramaBg = Crafty.e('2D, Canvas, Color')
+		.attr({ x: 130, y: 15, w: rect._w, h: rect._h, z: 999, alpha: 0.5 })
+		.color('rgb(0, 0, 0)');
+		// 设置文字显示区域
+		this._dramaText = Crafty.e('dramaDialog')
+		.attr({ w: 840, h: 150, x: 10, y: 400 })
+		.color('#ffffff')
+		.css('padding', '20');
+		
+		this._dramaBeginEvent = [];
+		this._dramaFinishEvent = [];
+		
+	},
+	
+	dramaSetup: function( charObj, script ){
+		this.setDramaChar( charObj );
+		this._dramaScript = script;
+		return this;
+	},
+	
+	setDramaChar: function( charObj ){
+		if( Crafty.$getType( charObj ) == 'Object' ){
+			var name;
+			for( name in charObj ){
+				this._dramaCharList[ name ] = charObj[ name ];
+				charObj[ name ]._charName = name;
+			}
+		}
+		else {
+			Crafty.$e.log('setDramaChar: 参数有误！');
+		}
+	},
+	
+	/**
+	 * 开始执行剧情
+	 */
+	playDrama: function(){
+		
+		if( this._dramaScript && !this._isDramaPlaying ){
+			this.showDrama();
+			this._isDramaPlaying = true;
+			// 执行剧情开始前事件
+			var i;
+			for( i = 0; this._dramaBeginEvent[ i ]; i++ ){
+				this._dramaBeginEvent[ i ]();
+			}
+			// 绑定剧情播放事件
+			Crafty.addEvent(this, Crafty.stage.elem, "mousedown", this.palyNextDrama );
+			this.palyNextDrama( { button: 0 });
+		}
+		else {
+			this.hideDrama();
+			this._isDramaPlaying = false;
+			Crafty.removeEvent(this, Crafty.stage.elem, "mousedown", this.palyNextDrama );
+		}
+	},
+	
+	hideDrama: function(){
+		// 隐藏所有的角色
+		var name;
+		for( name in this._dramaCharList ){
+			this._dramaCharList[ name ].attr('visible', false); 
+		}
+		// 隐藏名称
+		this._dramaText.hideDialog();
+		// 隐藏背景
+		this._dramaBg.attr('visible', false );
+	},
+	
+	showDrama: function(){
+		// 显示所有的角色
+		var name;
+		for( name in this._dramaCharList ){
+			this._dramaCharList[ name ].attr('visible', true); 
+		}
+		// 显示名称
+		this._dramaText.showDialog;
+		// 显示背景
+		this._dramaBg.attr('visible', true );
+	},
+	
+	palyNextDrama: function( e ){
+		if( e.button == 0 ){
+			if( this._dramaScript && this._isDramaPlaying ){
+				// 剧情尚未结束
+				if( this._curDramaIndex < this._dramaScript.length ){
+					// 获取下一幕内容
+					var d = this._dramaScript[ this._curDramaIndex ],
+					name = d.name,
+					face = d.face,
+					text = d.text,
+					action = d.action,
+					pos = d.pos,
+					char = this._dramaCharList[ name ],
+					_this = this, oldChar, idleChar;
+					if( char && face && text && pos ){
+						// 设置其位置
+						if( pos == 'left' ){
+							char.attr({ x: 150, y:100 });
+							oldChar = this._curLeftChar;
+							idleChar = this._curRightChar
+							this._curLeftChar = char;
+						}
+						else {
+							char.attr({ x: 700, y:100 });
+							oldChar = this._curRightChar;
+							idleChar = this._curLeftChar
+							this._curRightChar = char;
+						}
+						// 对相对方向的角色设置为透明度0.5
+						if( idleChar ){
+							idleChar.attr('alpha', 0.5);
+						}
+						Crafty.removeEvent( this, Crafty.stage.elem, "mousedown", this.palyNextDrama );
+						// 若当前位置已经有角色存在，则先隐藏该角色
+						if( oldChar && oldChar != char  ){
+						
+							oldChar.charFadeOut( function(){
+								
+								char.charFadeIn( function(){
+									char.setFace( face );
+									_this._dramaText.setDialog( char, text, pos );
+									Crafty.addEvent( _this, Crafty.stage.elem, "mousedown", _this.palyNextDrama );
+								});
+							});
+						}
+						else {
+							char.charFadeIn( function(){
+								char.setFace( face );	
+								_this._dramaText.setDialog( char, text, pos );
+								Crafty.addEvent( _this, Crafty.stage.elem, "mousedown", _this.palyNextDrama );
+							});
+						}
+						
+						this._curDramaIndex++;
+					}
+					else {
+						Crafty.$e.exit('palyNextDrama: 指定角色[ ' + name + ' ]不存在！');
+					}
+				}
+				else {
+					this.hideDrama();
+					this._isDramaPlaying = false;
+					this._curDramaIndex = 0;
+					Crafty.removeEvent(this, Crafty.stage.elem, "mousedown", this.palyNextDrama );
+					// 执行剧情结束事件事件
+					var i;
+					for( i = 0; this._dramaFinishEvent[ i ]; i++ ){
+						this._dramaFinishEvent[ i ]();
+					}
+					this.trigger('dramaFinished');
+				}
+			}
+		}
+	},
+	
+	/**
+	 * 设置背景
+	 */
+	setDramaBg: function( name, alpha ){
+		if( name ){
+			if( this._dramaBgSprite ){
+				this._dramaBg.removeComponent( this._dramaBgSprite );
+			}
+			this._dramaBg.addComponent( name );
+			this._dramaBgSprite = name;
+		}
+		if( alpha ){
+			this._dramaBg.attr({alpha: alpha});
+		}
+		return this;
+	},
+	
+	onDramaBegin: function( fn ){
+		if( Crafty.$getType( fn ) == 'Function' ){
+			this._dramaBeginEvent.push( fn );
+		}
+		else {
+			$Crafty.$e.log('onDramaBegin: 参数有误！');
+		}
+		return this;
+	},
+	
+	onDramaFinished: function( fn ){
+		if( Crafty.$getType( fn ) == 'Function' ){
+			this._dramaFinishEvent.push( fn );
+		}
+		else {
+			$Crafty.$e.log('onDramaFinished: 参数有误！');
+		}
+		return this;
+	}
+	
+});
+
